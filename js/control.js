4e9e13907273cc90802efa2ebe9334f23f42bba8
Vickkk <justusofnigeria@mail.com>
2022-10-20 21:34:13 +0100

    refactored 1

--- js/control.js
+++ js/control.js
@@ -4,14 +4,24 @@ const default_input = document.querySelector("form input[type='file']");
 const themeToggler = document.querySelector(".nav-theme-toggler .button-toggler");
 const em = themeToggler.children.item("em");
 const theme = localStorage.getItem("theme");
+const icontheme = localStorage.getItem("icontheme");
 
 custom_button.addEventListener("click", callCustomInput);
+window.addEventListener("load", function () {
+	body.classList.toggle(theme === "dark" ? "dark" : "light");
+	em.classList.toggle(icontheme === "bi-sun-fill" ? "bi-sun-fill" : "bi-moon-fill");
+});
 
 function callCustomInput(e) {
 	e.preventDefault();
 	e.stopPropagation();
 	default_input.click();
 }
+
+// for refactoring, use the object method on localStorage
+// const themeChange = {
+// 	light: {},
+// };
 
 themeToggler.addEventListener("click", function (e) {
 	e.stopPropagation();
@@ -21,10 +31,14 @@ themeToggler.addEventListener("click", function (e) {
 		em.classList.add("bi-sun-fill");
 		body.classList.remove("light");
 		body.classList.add("dark");
+		localStorage.setItem("theme", "dark");
+		localStorage.setItem("icontheme", "bi-sun-fill");
 	} else {
 		em.classList.add("bi-moon-fill");
 		em.classList.remove("bi-sun-fill");
 		body.classList.remove("dark");
 		body.classList.add("light");
+		localStorage.setItem("theme", "light");
+		localStorage.setItem("icontheme", "bi-moon-fill");
 	}
 });
