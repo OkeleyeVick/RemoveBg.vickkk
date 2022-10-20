a913aaf93a048c013e0947779d2a9419aed7061c
Vickkk <justusofnigeria@mail.com>
2022-10-20 20:55:06 +0100

    Changes made

--- js/control.js
+++ js/control.js
@@ -3,40 +3,28 @@ const body = document.body;
 const default_input = document.querySelector("form input[type='file']");
 const themeToggler = document.querySelector(".nav-theme-toggler .button-toggler");
 const em = themeToggler.children.item("em");
+const theme = localStorage.getItem("theme");
 
-window.addEventListener("load", function () {
-	checkTheme();
+custom_button.addEventListener("click", callCustomInput);
 
-	custom_button.addEventListener("click", callCustomInput);
-	themeToggler.addEventListener("click", ToggleTheme);
+function callCustomInput(e) {
+	e.preventDefault();
+	e.stopPropagation();
+	default_input.click();
+}
 
-	// function to toggle call the custom input
-	function callCustomInput(e) {
-		e.preventDefault();
-		e.stopPropagation();
-		default_input.click();
-	}
-
-	// function to light/dark mode
-	function ToggleTheme(e) {
-		e.stopPropagation();
-		if (em.classList.contains("bi-moon-fill")) {
-			em.classList.remove("bi-moon-fill");
-			em.classList.add("bi-sun-fill");
-			localStorage.setItem("theme", "dark");
-		} else {
-			em.classList.add("bi-moon-fill");
-			em.classList.remove("bi-sun-fill");
-			localStorage.setItem("theme", "light");
-		}
-	}
-
-	// check if there is a theme
-	function checkTheme() {
-		const themeFromStorage = localStorage.getItem("theme");
-
-		if (themeFromStorage !== null && themeFromStorage) {
-			body.classList.add(localStorage.getItem("theme"));
-		}
+themeToggler.addEventListener("click", function (e) {
+	e.stopPropagation();
+	e.preventDefault();
+	if (em.classList.contains("bi-moon-fill")) {
+		em.classList.remove("bi-moon-fill");
+		em.classList.add("bi-sun-fill");
+		body.classList.remove("light");
+		body.classList.add("dark");
+	} else {
+		em.classList.add("bi-moon-fill");
+		em.classList.remove("bi-sun-fill");
+		body.classList.remove("dark");
+		body.classList.add("light");
 	}
 });
