f011a31bf832b4694626212e5f6dc969dbf7db7b
Vickkk <justusofnigeria@mail.com>
2022-10-20 16:39:11 +0100

    ...

--- js/control.js
+++ js/control.js
@@ -2,29 +2,41 @@ const custom_button = document.querySelector("._custom-button button");
 const body = document.body;
 const default_input = document.querySelector("form input[type='file']");
 const themeToggler = document.querySelector(".nav-theme-toggler .button-toggler");
+const em = themeToggler.children.item("em");
 
-custom_button.addEventListener("click", callCustomInput);
-themeToggler.addEventListener("click", ToggleTheme);
+window.addEventListener("load", function () {
+	checkTheme();
 
-// function to toggle call the custom input
-function callCustomInput(e) {
-	e.preventDefault();
-	e.stopPropagation();
-	default_input.click();
-}
+	custom_button.addEventListener("click", callCustomInput);
+	themeToggler.addEventListener("click", ToggleTheme);
 
-// function to light/dark mode
-function ToggleTheme(e) {
-	e.stopPropagation();
-	const em = themeToggler.children.item("em");
-	if (em.classList.contains("bi-moon-fill")) {
-		em.classList.remove("bi-moon-fill");
-		em.classList.add("bi-sun-fill");
-		body.classList.add("dark");
-		// function to set the Theme in storage
-	} else {
-		em.classList.add("bi-moon-fill");
-		em.classList.remove("bi-sun-fill");
-		body.classList.remove("dark");
+	// function to toggle call the custom input
+	function callCustomInput(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		default_input.click();
 	}
-}
+
+	// function to light/dark mode
+	function ToggleTheme(e) {
+		e.stopPropagation();
+		if (em.classList.contains("bi-moon-fill")) {
+			em.classList.remove("bi-moon-fill");
+			em.classList.add("bi-sun-fill");
+			localStorage.setItem("theme", "dark");
+		} else {
+			em.classList.add("bi-moon-fill");
+			em.classList.remove("bi-sun-fill");
+			localStorage.setItem("theme", "light");
+		}
+	}
+
+	// check if there is a theme
+	function checkTheme() {
+		const themeFromStorage = localStorage.getItem("theme");
+
+		if (themeFromStorage !== null && themeFromStorage) {
+			body.classList.add(localStorage.getItem("theme"));
+		}
+	}
+});
