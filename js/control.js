96837def5291275259ac1e345f18770819c6b2dd
Vickkk <justusofnigeria@gmail.com>
2022-10-21 00:18:44 +0100

    Futher refactoring

--- js/control.js
+++ js/control.js
@@ -42,3 +42,12 @@ themeToggler.addEventListener("click", function (e) {
 		localStorage.setItem("icontheme", "bi-moon-fill");
 	}
 });
+
+// create a div  that shows that the image has been added
+
+// use tabs and pills to check between original image and background removed image
+// in the background-removed image, add a download button
+
+const _containerInner = document.querySelector("._container-inner");
+const _imageWrapper = document.createElement("div");
+_imageWrapper.classList.add("_image-wrapper");
